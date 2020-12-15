7449551883a80d12cb14c4ab733f25168d521a3e
Irina Murashka <irvil@tut.by>
2020-12-15 00:15:50 +0000

    mob-menu

--- js/menu.js
+++ js/menu.js
@@ -1,7 +1,9 @@
 (() => {
     const menuBtnRef = document.querySelector("[data-menu-button]");
+    const menuRef = document.querySelector("[data-menu]");
 
     menuBtnRef.addEventListener("click", () => {
         menuBtnRef.classList.toggle("is-open");
+        menuRef.classList.toggle("is-open");
     });
 })();
